0041fe64eb36b3156b9309b23dc61b2e925205bd
paloblanco <panella.rocco@gmail.com>
2021-10-24 12:38:46 -0400

    moving light and moving block setup

--- main.lua
+++ main.lua
@@ -4,7 +4,10 @@ function lovr.load()
       
     camx, camy, camz = 0,2,5
     tx,ty,tz = 0,0,0
+    lx,ly,lz = 0,-0.5,3
+    ldist=2
     angle = 0
+    anglel = 0
 
     local camfrom = lovr.math.vec3(camx,camy,camz)
     local camto = lovr.math.vec3(tx,ty,tz)
@@ -17,10 +20,33 @@ function lovr.load()
     lovr.graphics.setViewPose(1,cammat,true)
     shader:send('lightPos', {camx,camy,camz})
     matrix = camme
+
+    keypress = false
 end
-  
+
+function lovr.keypressed(key)
+    if key=='space' then keypress = true end
+end
+function lovr.keyreleased(key)
+    if key=='space' then keypress = false end
+end
+
+
 function lovr.update(dt)
-    angle = angle + dt
+    
+    
+    if keypress then
+        angle = angle + dt
+    end
+
+    anglel = anglel + dt
+
+    lz = ldist * math.sin(anglel)
+    ly = -.5
+    lx = ldist * math.cos(anglel)
+
+    shader:send('lightPos', {lx,ly,lz})
+
 end
 
 function lovr.draw()
@@ -30,4 +56,16 @@ function lovr.draw()
                         tx,ty,tz,
                         2,
                         angle,0,1,0)
+    lovr.graphics.setColor(0.5,0.5,1,1)
+                        lovr.graphics.cube('fill',
+                        tx,ty-6,tz-3,
+                        8,
+                        0,0,1,0)
+    lovr.graphics.setColor(1,1,1,1)
+    lovr.graphics.cube('fill',
+                        lx,ly,lz,
+                        .25,
+                        0,0,1,0)
+                    
+
 end
--- shader.lua
+++ shader.lua
@@ -1,67 +1,15 @@
-customVertex = [[
-    vec4 position(mat4 projection, mat4 transform, vec4 vertex)
-    {
-        return projection * transform * vertex;
-    }
-]]
-
-defaultVertex = [[
-    out vec3 FragmentPos;
-    out vec3 Normal;
-
-    vec4 position(mat4 projection, mat4 transform, vec4 vertex) 
-    { 
-        Normal = lovrNormal * lovrNormalMatrix;
-        FragmentPos = vec3(lovrModel * vertex);
-        
-        return projection * transform * vertex; 
-    }
-]]
-
--- customFragment = [[
---     vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) 
---     {
---         return graphicsColor * lovrDiffuseColor * vertexColor * texture(image, uv);
---         }
--- ]]
-
-customFragment = [[
-    uniform vec4 ambience;
-    vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) 
-    {
-        //object color
-        vec4 baseColor = graphicsColor * texture(image, uv);
-        return baseColor * ambience;
-    }
-]]
-
-defaultFragment = [[
-    uniform vec4 ambience;
-    
-    uniform vec4 liteColor;
-    uniform vec3 lightPos;
-
-    in vec3 Normal;
-    in vec3 FragmentPos;
-    
-    vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) 
-    {    
-        //diffuse
-        vec3 norm = normalize(Normal);
-        vec3 lightDir = normalize(lightPos - FragmentPos);
-        float diff = max(dot(norm, lightDir), 0.0);
-        vec4 diffuse = diff * liteColor;
-                        
-        vec4 baseColor = graphicsColor * texture(image, uv);            
-        return baseColor * (ambience + diffuse);
-    }
-]]
+defaultVertex = lovr.filesystem.read("shaderv.vs")
+defaultFragment = lovr.filesystem.read("shaderf.fs")
 
 shader = lovr.graphics.newShader(defaultVertex, defaultFragment, {})
 
 shader:send('ambience', { 0.2, 0.2, 0.2, 1.0 })
 shader:send('liteColor', {1.0,1.0,1.0,1.0})
-shader:send('lightPos', {2.0,5.0,0})
+shader:send('lightPos', {0.0,2.0,5.0})
+
+shader:send('specularStrength', 0.5)
+shader:send('metallic',32.0)
+shader:send('viewPos',{0.0,2.0,5.0})
 
 
 return shader
