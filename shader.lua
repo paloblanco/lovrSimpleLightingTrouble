339b5a73eb7c0ec2a683812b79a9924f2801a525
paloblanco <panella.rocco@gmail.com>
2021-10-25 13:30:15 -0400

    shaders working in their own ways

--- shader.lua
+++ shader.lua
@@ -1,12 +1,12 @@
-vertexFile = "shaderv.vs"
-fragmentFile = "shaderf.fs"
+vertexFile = "shaderv2.vs"
+fragmentFile = "shaderf2.fs"
 
 defaultVertex = lovr.filesystem.read(vertexFile)
 defaultFragment = lovr.filesystem.read(fragmentFile)
 
 shader = lovr.graphics.newShader(defaultVertex, defaultFragment, {})
 
-shader:send('ambience', { 0.2, 0.2, 0.2, 1.0 })
+shader:send('ambience', { 0.05, 0.05, 0.05, 1.0 })
 shader:send('liteColor', {1.0,1.0,1.0,1.0})
 shader:send('lightPos', {0.0,2.0,5.0})
 
