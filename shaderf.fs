cfb8651335f5473d0313184d10be7c452d7f6a7b
paloblanco <45581243+paloblanco@users.noreply.github.com>
2022-01-02 15:31:02 -0500

    reordering complete and shader looking good

--- shaderf.fs
+++ shaderf.fs
@@ -1,32 +1,29 @@
-in vec3 lightDirection;
-in vec3 normalDirection;
-in vec3 vertexPosition;
-in vec3 FragmentPos;
+    uniform vec4 ambience;
 
-uniform vec4 ambience;
-uniform vec4 liteColor;
-in vec3 LightPos;
+    uniform vec4 liteColor;
+    uniform vec3 lightPos;
 
-uniform vec3 viewPos;
-uniform float specularStrength;
-uniform float metallic;
+    in vec3 Normal;
+    in vec3 FragmentPos;
 
-vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) {
-    
-    
-    //diffuse    
-    float diff = max(dot(normalDirection, lightDirection), 0.);
-    vec4 diffuse = diff * liteColor;
-
-
-    // specular
-    vec3 viewDirection = normalize(viewPos - FragmentPos);
-    vec3 lightDir2 = normalize(LightPos - FragmentPos);
-    vec3 r = reflect(-lightDir2, normalDirection);
-    float specularAngle = max(dot(r, viewDirection), 0.);
-    float spec = pow(specularAngle, metallic);
-    vec4 specular = spec * liteColor * specularStrength;
-  
-    vec4 baseColor = graphicsColor * texture(image, uv);            
-    return baseColor * (ambience + diffuse + specular);
-}
+    uniform vec3 viewPos;
+    uniform float specularStrength;
+    uniform int metallic;
+        
+    vec4 color(vec4 graphicsColor, sampler2D image, vec2 uv) 
+    {    
+        //diffuse
+        vec3 norm = normalize(Normal);
+        vec3 lightDir = normalize(lightPos - FragmentPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec4 diffuse = diff * liteColor;
+            
+        //specular
+        vec3 viewDir = normalize(viewPos - FragmentPos);
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), metallic);
+        vec4 specular = specularStrength * spec * liteColor;
+            
+        vec4 baseColor = graphicsColor * texture(image, uv);            
+        return baseColor * (ambience + diffuse + specular);
+    }
